ebdc9a9dce798304df6749ac3f05091ab3593c2d
ats1999 <rkt82224@gmail.com>
2024-12-13 06:04:29 +0530

    created simple proxy

---
+++ test_server/s2.js
@@ -0,0 +1,17 @@
+const http = require("http");
+
+// Create the HTTP server
+const server = http.createServer((req, res) => {
+  console.log("Request Received");
+  // Set the response header
+  res.writeHead(200, { "Content-Type": "text/plain" });
+
+  // Send the response body
+  res.end("Hello, World!\n");
+});
+
+// Start the server on port 8080
+const PORT = 8081;
+server.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
