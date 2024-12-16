817fadc99904c05e7cf8e68e5180f315a3bec82a
ats1999 <rkt82224@gmail.com>
2024-12-16 13:41:32 +0530

    added PatternType in Route

--- src/config/location.rs
+++ src/config/location.rs
@@ -1,9 +1,17 @@
 use serde;
 
 #[derive(Clone, Debug, serde::Serialize, serde::Deserialize)]
+pub enum PatternType {
+    Text,
+    Wildcard,
+    Regex,
+}
+
+#[derive(Clone, Debug, serde::Serialize, serde::Deserialize)]
 pub struct Route {
     // TODO: add support for wild card paths
     // TODO: add support for regex path matching
     pub pattern: String,
+    pub pattern_type: PatternType,
     pub backend: String,
 }
