c9c11f7d3d568950afcc491f77d039b7af1e34bb
ats1999 <rkt82224@gmail.com>
2024-12-29 18:16:21 +0530

    moved dakia ascii art to file

--- dakia/build.rs
+++ dakia/build.rs
@@ -17,9 +17,36 @@ fn main() {
 
     // Output the ASCII art to a file (build.rs will use this in the main code)
     let out_dir = env::var("OUT_DIR").unwrap();
-    let path = format!("{}/ascii_version.rs", out_dir);
+    let ascii_version_path = format!("{}/ascii_version.rs", out_dir);
 
-    let mut file = File::create(path).unwrap();
+    let mut file = File::create(ascii_version_path).unwrap();
     file.write_all(format!("pub const ASCII_VERSION: &str = r#\"{}\"#;", ascii_version).as_bytes())
         .unwrap();
+
+    // write dakia ascii art
+    const DAKIA_ASCII_ART: &str = "
+        _______
+        \\  ___ `'.                    .          .--.
+         ' |--.\\  \\                 .'|          |__|
+         | |    \\  '              .'  |          .--.
+         | |     |  '     __     <    |          |  |     __
+         | |     |  |  .:--.'.    |   | ____     |  |  .:--.'.
+         | |     ' .' / |   \\ |   |   | \\ .'     |  | / |   \\ |
+         | |___.' /'  `\" __ | |   |   |/  .      |  | `\" __ | |
+        /_______.'/    .'.''| |   |    /\\  \\     |__|  .'.''| |
+        \\_______|/    / /   | |_  |   |  \\  \\         / /   | |_ 
+                      \\ \\._,\\ '/  '    \\  \\  \\        \\ \\._,\\ '/
+                       `--'  `\"  '------'  '---'       `--'  `\"";
+
+    let dakia_ascii_art_path = format!("{}/dakia_ascii_art.rs", out_dir);
+
+    let mut file = File::create(dakia_ascii_art_path).unwrap();
+    file.write_all(
+        format!(
+            "pub const DAKIA_ASCII_ART: &str = r#\"{}\"#;",
+            DAKIA_ASCII_ART
+        )
+        .as_bytes(),
+    )
+    .unwrap();
 }
--- dakia/src/shared/common.rs
+++ dakia/src/shared/common.rs
@@ -1,22 +1,11 @@
 include!(concat!(env!("OUT_DIR"), "/ascii_version.rs"));
+include!(concat!(env!("OUT_DIR"), "/dakia_ascii_art.rs"));
+
 pub fn exit() {
     std::process::exit(0);
 }
 
 pub fn get_dakia_ascii_art() -> String {
-    const DAKIA_ASCII_ART: &str = "
-_______
-\\  ___ `'.                    .          .--.
- ' |--.\\  \\                 .'|          |__|
- | |    \\  '              .'  |          .--.
- | |     |  '     __     <    |          |  |     __
- | |     |  |  .:--.'.    |   | ____     |  |  .:--.'.
- | |     ' .' / |   \\ |   |   | \\ .'     |  | / |   \\ |
- | |___.' /'  `\" __ | |   |   |/  .      |  | `\" __ | |
-/_______.'/    .'.''| |   |    /\\  \\     |__|  .'.''| |
-\\_______|/    / /   | |_  |   |  \\  \\         / /   | |_ 
-              \\ \\._,\\ '/  '    \\  \\  \\        \\ \\._,\\ '/
-               `--'  `\"  '------'  '---'       `--'  `\"";
     DAKIA_ASCII_ART.to_string() + "\n\n" + get_ascii_version()
 }
 
