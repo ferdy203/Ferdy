43c247cf8aab269141ed263323ccb4a6054f8adc
ats1999 <rkt82224@gmail.com>
2025-03-02 21:25:34 +0530

    fix: fixed version interceptor for trait

--- dakia/src/gateway/interceptors/version/interceptor.rs
+++ dakia/src/gateway/interceptors/version/interceptor.rs
@@ -1,6 +1,6 @@
 use crate::{
     error::DakiaResult,
-    gateway::interceptor::{Hook, HookMask, Interceptor, InterceptorName},
+    gateway::interceptor::{Hook, HookMask, Interceptor, InterceptorName, PhaseMask, PhaseResult},
     proxy::http::Session,
 };
 
@@ -11,12 +11,31 @@ impl Interceptor for VersionInterceptor {
         InterceptorName::Version
     }
 
-    fn hook(&self) -> Option<HookMask> {
+    fn hook_mask(&self) -> Option<HookMask> {
         Some(Hook::PreDownstreamResponseHeaderFlush.mask())
     }
 
     fn filter(&self, _session: &Session) -> DakiaResult<bool> {
-        todo!()
+        Ok(true)
     }
-    //
+
+    fn request_filter(&self, _session: &Session) -> PhaseResult {
+        Ok(false.into())
+    }
+
+    fn upstream_proxy_filter(&self, _session: &Session) -> PhaseResult {
+        Ok(false.into())
+    }
+
+    fn pre_upstream_request(&self, _session: &Session) -> PhaseResult {
+        Ok(false.into())
+    }
+
+    fn post_upstream_response(&self, _session: &Session) -> PhaseResult {
+        Ok(false.into())
+    }
+
+    fn phase_mask(&self) -> Option<PhaseMask> {
+        None
+    }
 }
